dd4aabfdd21e588625f36bb61ce834f60da2bfa2
Alex Yin <fudanyinxin@gmail.com>
2018-05-21 17:34:20 +1000

    Add button 1

---
+++ wp-content/themes/wplibrary/templates/button-1.php
@@ -0,0 +1,16 @@
+<?php
+// Template Name: button-1
+
+the_post();
+get_header(); 
+?>
+
+<div class="page">
+
+  <a href="#" class="button-1">
+    <span>button-1</span>
+  </a>
+
+</div>
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/wplibrary/templates/full-screen-search-modal.php
@@ -0,0 +1,15 @@
+<?php
+// Template Name: full-screen-search-modal
+
+
+the_post();
+get_header(); 
+?>
+
+<div class="page">
+
+  good start!!!!!
+
+</div>
+
+<?php get_footer(); ?>
